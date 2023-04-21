69210dfdd7fe0c95325f66c74a723bd95c0f09be
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-21 16:41:30 +0200

    Update deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -1,12 +1,14 @@
 # Get files from Main
 echo "Get files from Main"
-echo ${{ github.workspace }}
+#echo ${{ github.workspace }}
 
 # Replace file on VPS  
 #scp -r [path]main.py root@68.183.15.232:/home/farm
 
 # Pull github code to here
+echo "Git pull"
 git pull
 
 # Restart the application after we've pulled in new code
+echo "restart application"
 systemctl restart farm
