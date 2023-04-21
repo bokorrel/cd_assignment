099337b35f32285da90464bf4a8ace63bae46bab
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-21 16:46:13 +0200

    Update deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -5,6 +5,10 @@ echo "Get files from Main"
 # Replace file on VPS  
 #scp -r [path]main.py root@68.183.15.232:/home/farm
 
+# Navigate to repository on droplet
+cd /home/cd_assignment
+echo pwd
+
 # Pull github code to here
 echo "Git pull"
 git pull
