a3d8866b1c0c9456d9b2ed93ce0f2e35951265d8
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-21 16:22:37 +0200

    Update deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -1,7 +1,6 @@
 # Get files from Main
 echo "Get files from Main"
 echo ${{ github.workspace }}
-echo $GITHUB_WORKSPACE
 
 # Replace file on VPS  
 #scp -r [path]main.py root@68.183.15.232:/home/farm
