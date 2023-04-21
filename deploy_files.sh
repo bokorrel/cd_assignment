205e1817c51b246fe0f85f954525b71cd1c70388
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-21 15:38:26 +0200

    Update deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -6,5 +6,8 @@ echo $GITHUB_WORKSPACE
 # Replace file on VPS  
 #scp -r [path]main.py root@68.183.15.232:/home/farm
 
-# Restart the application after we've pulled in new code, otherwise we can be looking for what went wrong for quite a while..
-# systemctl restart farm
+# Pull github code to here
+git pull
+
+# Restart the application after we've pulled in new code
+systemctl restart farm
