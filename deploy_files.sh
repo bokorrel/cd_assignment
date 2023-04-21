ee30550c398417f993686ce6aab16119f33cf6a8
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-21 14:07:48 +0200

    Update and rename deploy-to-VPS.sh to deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -1,9 +1,10 @@
-# Access the VPS
-# @ssh root@68.183.15.232
-
+# Get files from Main
+echo "Get files from Main"
+echo ${{ github.workspace }}
+echo $GITHUB_WORKSPACE
 
 # Replace file on VPS  
-# scp -r [path]main.py root@68.183.15.232:/home/farm
+#scp -r [path]main.py root@68.183.15.232:/home/farm
 
 # Restart the application after we've pulled in new code, otherwise we can be looking for what went wrong for quite a while..
 # systemctl restart farm
