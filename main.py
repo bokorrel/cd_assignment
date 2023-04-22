a09eec7aea82a35e9814d73190763f1d954dfa7e
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-04-22 21:08:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, this is my pretty application!'
+    #return 'Hello, this is my pretty application!'
+    return render_template("index.html", title="Index")
 
 @app.route('/final')
 def final():
